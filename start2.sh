ac5c49a25b5d9aa03e637c7026cc6e327b2f6ad8
floriankirmaier <florian.kirmaier@gmail.com>
2020-03-06 18:43:18 +0400

    test

--- start2.sh
+++ start2.sh
@@ -13,7 +13,7 @@ fi
 
 CLASSPATH=$(JARS=("$(pwd)/libs"/*.jar "$(pwd)/jfx/$PLATFORM/"*.jar); IFS=:; echo "${JARS[*]}")
 JPROCLASSPATH=$(JARS=("$(pwd)/jprolibs"/*.jar); IFS=:; echo "${JARS[*]}")
-JPRO_ARGS=("-Xmx1000m" "-Djpro.applications.default=com.jpro.hellojpro.HelloJProFXML" "-Dprism.useFontConfig=false" "-Djpro.forceShutdown=true" "-Dhttp.port=8080" "-Dquantum.norenderjobs=true" "-Dglass.platform=Monocle" "-Dmonocle.platform=Headless" "-Dprism.fontdir=fonts/" "-Djpro.deployment=GRADLE-Distribution") 
+JPRO_ARGS=("-Xmx1000m" "-Djpro.applications.default=com.jpro.hellojpro.HelloJProFXML" "-Dprism.useFontConfig=false" "-Djpro.forceShutdown=true" "-Dhttp.port=80" "-Dquantum.norenderjobs=true" "-Dglass.platform=Monocle" "-Dmonocle.platform=Headless" "-Dprism.fontdir=fonts/" "-Djpro.deployment=GRADLE-Distribution")
 
 
 
